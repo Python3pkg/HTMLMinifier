4cf4ab54ab5751f8e9a86c0c75128ce6f64fa9a1
Chi-En Wu <jason2506.mail@gmail.com>
2014-02-05 22:48:25 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='HTMLMinifier',
+    author='Chi-En Wu',
+    author_email='',
+    description='A simple HTML5 minifier written in Python.',
+    long_description=__doc__,
+    packages=['HTMLMinifier'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=['six']
+)
